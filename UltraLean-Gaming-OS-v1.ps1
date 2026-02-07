4a758a7cfcd241540e3a1d90d6fff7fd28158abc
1245ym <yusufmulla13@icloud.com>
2026-02-07 15:20:12 +0400

    Update UltraLean-Gaming-OS-v1.ps1

--- UltraLean-Gaming-OS-v1.ps1
+++ UltraLean-Gaming-OS-v1.ps1
@@ -1,4 +1,5 @@
 UltraLean-Gaming-OS-v1 for powershell(run as admin,just copy and paste:it is tested and fully safe)
+TO RUN THE CODE COPY AN DPASTE THIS INTO POWERSHELL AS ADMIN:irm https://raw.githubusercontent.com/1245ym/UltraLean-Gaming-Windows-Powershell-code-/main/UltraLean-Gaming-OS-v1.ps1 | iex
 
 =====================================================
 # 💀 GOD MODE ULTRA-GAMING DESKTOP WINDOWS 10 💀
